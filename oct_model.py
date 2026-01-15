37cc2e26646c30d486c11409cbaa9a8d09e5c467
Simonolek <olekhnovitch.simon@gmail.com>
2026-01-15 13:03:14 +0100

    Work on oct_model()

--- oct_model.py
+++ oct_model.py
@@ -3,6 +3,16 @@ import numpy as np
 import pandas as pd
 import pyomo.environ as pyo
 
-def oct_model():
-    return None
+def oct_model(D):
     
+    # Model
+    m = pyo.ConcreteModel("OCT")
+
+    # Params
+    m.T = pyo.Param(initialize = 2**(D+1)-1)
+
+    # Sets
+    m.TSET = pyo.RangeSet(1, (m.T)//2)
+    m.TBSET = pyo.RangeSet(1, (m.T)//2)
+    m.TLSET = pyo.RangeSet((m.T)//2 + 1, m.T)
+    return None
